ba383cd2f3e1a270b6d5128490c4b6aa713a0e01
scottcombs <scott@nutznboltllc.com>
2018-04-06 20:24:29 -0700

    Generate ICNS button now observes whether it has an iconset folder from a valid image available to make the icns file. It's not perfect, it should check for the actual folder available but it does check to see if there is an image set, which is the next best thing.

--- Iconset/ViewController.m
+++ Iconset/ViewController.m
@@ -14,6 +14,9 @@
 @synthesize imageView;
 @synthesize image;
 @synthesize url;
+@synthesize generateICNSFileButton;
+
+static void *ImageContext = &ImageContext;
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
@@ -23,8 +26,14 @@
 }
 
 - (void)awakeFromNib{
+	// Set ourselves to the AppDelegate so drag operations can
+	// find us easier
 	AppDelegate* ad = (AppDelegate*)[NSApp delegate];
 	ad.vc = self;
+	
+	// Add ourselves to observe the image property
+	[self addObserver:self forKeyPath:@"image" options:(NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld)
+				 context:ImageContext];
 }
 
 - (void)setRepresentedObject:(id)representedObject {
@@ -33,31 +42,59 @@
 	// Update the view, if already loaded.
 }
 
-- (IBAction)loadPNG:(id)sender {
-	NSOpenPanel* panel = [NSOpenPanel openPanel];
-	panel.directoryURL = [NSURL URLWithString:[NSString stringWithFormat:@"%@/Documents", NSHomeDirectory()]];
-	panel.title = @"Select File";
-	panel.message = @"Choose a PNG file of at least 1024px X 1024px";
-	panel.allowedFileTypes = @[@"png"];
-	panel.canChooseDirectories = NO;
-	panel.canChooseFiles = YES;
-	panel.allowsMultipleSelection = NO;
-	NSModalResponse returnCode = [panel runModal];
-	if (returnCode == NSModalResponseOK){
-		[self setPNGFromURL:panel.URL];
+/**
+ We observe when the image is set or unset to
+ enable or disable the generate icns button
+
+ @param keyPath Path to the image property
+ @param object Use (self)
+ @param change What kind of change
+ @param context Specifically the image context
+ */
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+	
+	if (context == ImageContext) {
+		// Check to see if image is null or new
+		if (self.image) {
+			if ([self.image.name isEqualToString:@"Default"]) {
+				// If we are the default image
+				self.generateICNSFileButton.enabled = NO;
+			}else{
+				// A good image to work with
+				self.generateICNSFileButton.enabled = YES;
+			}
+		}else{ // This is when the user deletes the image
+			// We need to do this in its own thread to work properly
+			__unused NSTimer* timer = [NSTimer scheduledTimerWithTimeInterval:0.3 repeats:NO block:^(NSTimer * _Nonnull timer) {
+				// Set the default image
+				self.image = [NSImage imageNamed:@"Default.png"];
+				// Force a refresh
+				[self.imageView setNeedsLayout:YES];
+				// Disable the button
+				self.generateICNSFileButton.enabled = NO;
+			}];
+		}
 	}
 }
 
+/**
+ Generate an iconset from a URL.
+
+ @param sender (unused)
+ */
 - (IBAction)generateIconset:(id)sender {
+	// Get all the parts we need from the URL
 	NSDictionary* components = [self components:self.url];
+	
+	// Find the SIPS app in our bundle
 	NSBundle* bundle = [NSBundle mainBundle];
 	NSFileManager* fm = [[NSFileManager alloc]init];
 	NSString* pathToSips = [bundle pathForResource:@"sips" ofType:@""];
-	// Make an iconset folder
 	
+	// Make an iconset folder
 	NSString* pathToIconset = [NSString stringWithFormat:@"%@%@.iconset", [components objectForKey:@"path"], [components objectForKey:@"filename"]];
 	
-	// Remove if exists
+	// Remove the old one if exists
 	NSError* err;
 	[fm removeItemAtPath:pathToIconset error:&err];
 	
@@ -71,6 +108,8 @@
 	// Continue if folder was made
 	BOOL isDir;
 	if ([fm fileExistsAtPath:pathToIconset isDirectory:&isDir]) {
+		
+		// Set up all our sizes
 		NSArray* array = @[@[@"16", @"icon_16x16.png"],
 						   @[@"32", @"icon_16x16@2x.png"],
 						   @[@"32", @"icon_32x32.png"],
@@ -82,9 +121,12 @@
 						   @[@"512", @"icon_512x512.png"],
 						   @[@"1024", @"icon_512x512@2x.png"]];
 		
+		// Run through each item in the array and build an icon
 		for (NSArray* item in array) {
 			NSString* size = item[0];
 			NSString* name = item[1];
+			
+			// Process through sips for a sized icon
 			NSTask* task = [[NSTask alloc]init];
 			task.launchPath = pathToSips;
 			NSString* arg1 = @"-Z";
@@ -97,17 +139,31 @@
 			[task waitUntilExit];
 		}
 	}
-	// End
+	// End of creating icons
 }
 
-- (NSDictionary*)components:(NSURL*)url{
+/**
+ Component is a convenience method to get all the pars
+ we need to rebuild a path and files. It gives us a
+ Dictionary for the path, filename and extension.
+
+ @param url The URL to decompose
+ @return NSDictionary containing the 3 components
+ */
+- (NSDictionary*)components:(NSURL*)url {
 	NSString* extension = [url.path pathExtension];
 	NSString* filename = self.url.lastPathComponent;
 	NSString* path = [url.path stringByReplacingOccurrencesOfString:filename withString:@""];
 	filename = [filename stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@".%@",extension] withString:@""];
+	
 	return @{@"path": path, @"filename": filename, @"extension": extension };
 }
 
+/**
+ Process the URL to interface with our app
+
+ @param sender Is a NSURL of the PNG file
+ */
 - (IBAction)setPNGFromURL:(id)sender {
 	NSURL* url = (NSURL*)sender;
 	self.url = [NSURL URLWithString:url.absoluteString];
@@ -116,22 +172,32 @@
 	[self generateIconset:self];
 }
 
-- (IBAction)createICNS:(id)sender{
+/**
+ Create an ICNS file from the iconset folder
+
+ @param sender (unused)
+ */
+- (IBAction)createICNS:(id)sender {
+	// Get the components of the iconset
 	NSDictionary* components = [self components:self.url];
+	
+	// Get the path of iconutil app in our bundle
 	NSBundle* bundle = [NSBundle mainBundle];
 	NSFileManager* fm = [[NSFileManager alloc]init];
 	NSString* pathToIconutil = [bundle pathForResource:@"iconutil" ofType:@""];
+	
+	// Construct path to match the iconset folder
 	NSString* pathToIconset = [NSString stringWithFormat:@"%@%@.iconset", [components objectForKey:@"path"], [components objectForKey:@"filename"]];
 	
-	// Path to icons
+	// Path to icns file to be made
 	NSString* icns = [NSString stringWithFormat:@"%@%@.icns",[components objectForKey:@"path"], [components objectForKey:@"filename"]];
 	
-	// Remove if necessary
+	// Remove old icns file if necessary
 	if ([fm fileExistsAtPath:icns]) {
 		[fm removeItemAtPath:icns error:NULL];
 	}
 	
-	// Create the icn set
+	// Create the icns file
 	NSTask* task = [[NSTask alloc]init];
 	task.launchPath = pathToIconutil;
 	NSString* arg1 = @"-c";
